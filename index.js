b0508c8c471284613b59516f92c5335abb1ce886
mahadi24t <mahadi24t@outlook.com>
2026-08-24 14:57:35 +0600

    custom object

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const menu = [
-    {name: "Margherita Pizza", price: 8.99},
-    {name: "Pepperoni Pizza", price: 9.99},
-    {name: "Veggie Pizza", price: 10.99},
-    {name: "BBQ Chicken Pizza", price: 11.99},
-    {name: "Hawaiian Pizza", price: 12.99}
+    { name: "Margherita Pizza", price: 8.99 },
+    { name: "Pepperoni Pizza", price: 9.99 },
+    { name: "Veggie Pizza", price: 10.99 },
+    { name: "BBQ Chicken Pizza", price: 11.99 },
+    { name: "Hawaiian Pizza", price: 12.99 }
 ];
-
 let cashInRegister = 100;
 const orderQueue = [];
 let nextOrderId = 1;
-
 function addNewPizza(pizzaObj) {
     menu.push(pizzaObj);
 }
-
 function placeOrder(pizzaName) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName);
     if (!selectedPizza) {
@@ -21,11 +18,10 @@ function placeOrder(pizzaName) {
         return undefined;
     }
     cashInRegister += selectedPizza.price;
-    const newOrder = {id: nextOrderId++, pizza: selectedPizza, status: "ordered"};
+    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" };
     orderQueue.push(newOrder);
     return newOrder;
 }
-
 function completeOrder(orderId) {
     const order = orderQueue.find(order => order.id === orderId);
     if (order) {
@@ -33,14 +29,12 @@ function completeOrder(orderId) {
     }
     return order;
 }
-
-addNewPizza({name: "Meat Lovers Pizza", price: 13.99});
-addNewPizza({name: "Buffalo Chicken Pizza", price: 14.99});
-addNewPizza({name: "Supreme Pizza", price: 15.99});
-
+addNewPizza({ name: "Meat Lovers Pizza", price: 13.99 });
+addNewPizza({ name: "Buffalo Chicken Pizza", price: 14.99 });
+addNewPizza({ name: "Supreme Pizza", price: 15.99 });
 placeOrder("Meat Lovers Pizza");
 completeOrder(1); // Pass number, not string
-
 console.log("Menu:", menu);
 console.log("Cash in Register:", cashInRegister);
 console.log("Order Queue:", orderQueue);
+export {};
---
+++ index1.ts
@@ -0,0 +1,17 @@
+type person = {
+    name: string,
+    age: number,
+    isStudent:boolean
+}
+
+let person1 = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2 = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
