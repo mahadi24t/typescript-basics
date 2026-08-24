d3608ed542dc8faaadbd0089eda7101fbd1912c5
mahadi24t <mahadi24t@outlook.com>
2026-08-24 16:38:50 +0600

    typing arrays

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ type Pizza = {
     name: string,
     price: number
 }
+
+type Order = {
+    id: number,
+    pizza: Pizza,
+    status:string
+}
 const menu = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
@@ -11,7 +17,7 @@ const menu = [
 
 let cashInRegister = 100
 let nextOrderId = 1
-const orderQueue = []
+const orderQueue: Order = []
 
 function addNewPizza(pizzaObj: Pizza) {
     menu.push(pizzaObj)
--- index1.ts
+++ index1.ts
@@ -10,13 +10,13 @@ type person = {
     address?: Address
 }
 
-let person1 = {
+let person1:person = {
     name: "Joe",
     age: 42,
     isStudent: true,
 }
 
-let person2 = {
+let person2:person = {
     name: "Jill",
     age: 66,
     isStudent: false,
@@ -31,4 +31,10 @@ function displayinfo(person: person) {
     console.log(`${person.name} lives in ${person.address?.street}`)
 }
 
-displayinfo(person2)
+// displayinfo(person2)
+
+// let ages: number[] =[100,101,102,103,104,105]
+// ages.push(106)
+
+let people: person[] = [person1, person2]
+
