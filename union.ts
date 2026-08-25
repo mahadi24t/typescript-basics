fbbc2c1c0f27bfd22d5a18c9c8df8a70ba348937
mahadi24t <mahadi24t@outlook.com>
2026-08-25 17:49:22 +0600

    git learn

--- union.ts
+++ union.ts
@@ -1,44 +1,83 @@
+// type User = {
+//     id: number
+//     username: string
+//     role: "member" | "contributor" | "admin"
+// }
+// type UserUpdate = Partial<User>
+
+// const users: User[] = [
+//     { id: 1, username: "john_doe", role: "member" },
+//     { id: 2, username: "jane_smith", role: "contributor" },
+//     { id: 3, username: "alice_jones", role: "admin" },
+//     { id: 4, username: "charlie_brown", role: "member" },
+// ];
+
+// function updateUser(id: number, updates: any) {
+//     // Find the user in the array by the id
+//     // Use Object.assign to update the found user in place. 
+//     // Check MDN if you need help with using Object.assign
+//     const foundUser = users.find(user => user.id === id)
+//     if (foundUser) {
+//         Object.assign(foundUser, updates)
+//     }
+//     else{
+//         console.error(`User with ID ${id} not found`)
+//         return
+//     }
+// }
+// function updatedUser(id: number, updates: UserUpdate) {
+//     const foundUser = users.find(user => user.id === id)
+//     if (foundUser) {
+//         Object.assign(foundUser, updates)
+//     }
+//     else{
+//         console.error(`User with ID ${id} not found`)
+//         return
+//     }
+// }
+// updatedUser(2, { username: "bro_smith" });
+// updatedUser(3, { role: "member" });
+// // Example updates:
+// updateUser(1, { username: "new_john_doe" });
+// updateUser(4, { role: "contributor" });
+
+// console.log(users)
+
 type User = {
     id: number
     username: string
     role: "member" | "contributor" | "admin"
 }
-type UserUpdate = Partial<User>
+
+type UpdatedUser = Partial<User>
+
+let nextUserId = 1
 
 const users: User[] = [
-    { id: 1, username: "john_doe", role: "member" },
-    { id: 2, username: "jane_smith", role: "contributor" },
-    { id: 3, username: "alice_jones", role: "admin" },
-    { id: 4, username: "charlie_brown", role: "member" },
+    { id: nextUserId++, username: "john_doe", role: "member" },
+    { id: nextUserId++, username: "jane_smith", role: "contributor" }
 ];
 
-function updateUser(id: number, updates: any) {
-    // Find the user in the array by the id
-    // Use Object.assign to update the found user in place. 
-    // Check MDN if you need help with using Object.assign
+function updateUser(id: number, updates: UpdatedUser) {
     const foundUser = users.find(user => user.id === id)
-    if (foundUser) {
-        Object.assign(foundUser, updates)
-    }
-    else{
-        console.error(`User with ID ${id} not found`)
+    if (!foundUser) {
+        console.error("User not found!")
         return
     }
+    Object.assign(foundUser, updates)
 }
-function updatedUser(id: number, updates: UserUpdate) {
-    const foundUser = users.find(user => user.id === id)
-    if (foundUser) {
-        Object.assign(foundUser, updates)
-    }
-    else{
-        console.error(`User with ID ${id} not found`)
-        return
-    }
+
+// updateUser(1, { username: "new_john_doe" });
+// updateUser(4, { role: "contributor" });
+
+function addNewUser(newUser: Omit<User,"id" | "user">): User {
+    const user: User = {  id: nextUserId++, ...newUser }
+    users.push(user)
+    return user
 }
-updatedUser(2, { username: "bro_smith" });
-updatedUser(3, { role: "member" });
-// Example updates:
-updateUser(1, { username: "new_john_doe" });
-updateUser(4, { role: "contributor" });
+}
+
+// example usage:
+addNewUser({ username: "joe_schmoe", role: "member" })
 
 console.log(users)
